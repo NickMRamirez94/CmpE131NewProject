b32f8f38cf818aaf00d774f7e3ac5fd57b300682
Nick Ramirez <bcgnick@gmail.com>
2017-11-24 20:38:30 -0800

    finished homepage

--- my_app/source/views.py
+++ my_app/source/views.py
@@ -119,183 +119,335 @@ def getPublisher(data_results):
 @my_view.route('/home')
 def greeting():
     return '''
+ <html lang="en">
+   <head>
+    <title>Gustoso</title>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<html>
-<head>
-<style>
-body {
-    font-family: "Lato", sans-serif;
-    transition: background-color .5s;
-}
+     <style>
 
-.about{
-	background-image: linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)),url(https://wallpaperscraft.com/image/book_table_sheets_pages_light_44458_2560x1600.jpg);
-	height: 100vh;
-	background-position: center;
-	background-size: cover;
+     .container {
+      width: 100%;
+      padding-right: 15px;
+      padding-left: 15px;
+      margin-right: auto;
+      margin-left: auto;
+    }
 
-}
+    @media (min-width: 576px) {
+      .container {
+        max-width: 540px;
+      }
+    }
 
-.sidenav {
-    height: 100%;
-    width: 0;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    background-color: #2C3539;
-    overflow-x: hidden;
-    transition: 0.5s;
-    padding-top: 60px;
-}
+    @media (min-width: 768px) {
+      .container {
+        max-width: 720px;
+      }
+    }
 
-.sidenav a {
-    padding: 8px 8px 8px 32px;
-    text-decoration: none;
-    font-size: 25px;
-    color: #FFFFFF;
-    display: block;
-    transition: 0.3s;
-}
+    @media (min-width: 992px) {
+      .container {
+        max-width: 960px;
+      }
+    }
 
-.sidenav a:hover, .offcanvas a:focus{
-    color: #F5F5F5;
-}
+    @media (min-width: 1200px) {
+      .container {
+        max-width: 1140px;
+      }
+    }
+     .banner {
+     	width: 100%;
+     	display: block;
 
-.sidenav .closebtn {
-    position: absolute;
-    top: 0;
-    right: 25px;
-    font-size: 36px;
-    margin-left: 50px;
-}
+     }
 
-#main {
-    transition: margin-left .5s;
-    padding: 16px;
-}
+     .banner > .banner-image{
+     	width: 100%;
+     	display: block;
+     }
 
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-</style>
-</head>
-<body class="about">
+     ul {
+         list-style-type: none;
+         margin: 0;
+         padding: 0;
+         overflow: hidden;
+         background-color: white;
+     }
 
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="http://127.0.0.1:5000/">Home</a>
-  <a href="http://127.0.0.1:5000/pc">Authors</a>
-  <a href="http://127.0.0.1:5000/playstation">Top Books</a>
-  <a href="http://127.0.0.1:5000/catalog">Catalog</a>
-</div>
+     li {
+         float: left;
+     }
 
-<div id="main">
-  <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
-</div>
+     li a {
+         display: block;
+         color: black;
+         text-align: center;
+         padding: 14px 16px;
+         text-decoration: none;
+     }
 
-<script>
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "white";
-}
+     li a:hover {
+         border: 1px solid #D2D4D5;
+         color: #7F7F7F
+     }
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-}
-</script>
+    .col-sm-6 {
+    -ms-flex: 0 0 50%;
+        flex: 0 0 50%;
+    max-width: 50%;
+    }
 
-</body>
-</html>
+    .col-6 {
+      -ms-flex: 0 0 50%;
+          flex: 0 0 50%;
+      max-width: 50%;
+    }
 
-<head>
-<title> Home </title>
-<head>
+    .col-sm-4 {
+        -ms-flex: 0 0 33.333333%;
+            flex: 0 0 33.333333%;
+        max-width: 33.333333%;
+      }
 
-<body style="background-color:FEFEFE;">
+    .col-1, .col-2, .col-3, .col-4, .col-5, .col-6, .col-7, .col-8, .col-9, .col-10, .col-11, .col-12, .col,
+    .col-auto, .col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12, .col-sm,
+    .col-sm-auto, .col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12, .col-md,
+    .col-md-auto, .col-lg-1, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg,
+    .col-lg-auto, .col-xl-1, .col-xl-2, .col-xl-3, .col-xl-4, .col-xl-5, .col-xl-6, .col-xl-7, .col-xl-8, .col-xl-9, .col-xl-10, .col-xl-11, .col-xl-12, .col-xl,
+    .col-xl-auto {
+      position: relative;
+      width: 100%;
+      min-height: 1px;
+      padding-right: 15px;
+      padding-left: 15px;
+    }
+
+    .col {
+      -ms-flex-preferred-size: 0;
+          flex-basis: 0;
+      -ms-flex-positive: 1;
+          flex-grow: 1;
+      max-width: 100%;
+    }
+
+    .row {
+      display: -ms-flexbox;
+      display: flex;
+      -ms-flex-wrap: wrap;
+          flex-wrap: wrap;
+      margin-right: -15px;
+      margin-left: -15px;
+    }
+
+    .text-center {
+      text-align: center !important;
+    }
+
+    h1{
+        font-family: "proxima-nova";
+        text-transform: uppercase;
+        font-weight: 600;
+        font-size: 2.5em;
+    }
+
+    h3{
+        font-weight: 100;
+    }
+
+    .top-buffer{
+        margin-top: 20px;
+    }
+
+    .top-buffer-large{
+        margin-top: 35px;
+    }
+
+    .top-buffer-xlarge{
+        margin-top: 100px;
+    }
+
+    .img-thumbnail {
+      padding: 0.25rem;
+      background-color: #fff;
+      border: 1px solid #ddd;
+      border-radius: 0.25rem;
+      transition: all 0.2s ease-in-out;
+      max-width: 100%;
+      height: 200px;
+    }
+
+    .img-thumbnail2 {
+      padding: 0.25rem;
+      background-color: #fff;
+      border: 1px solid #ddd;
+      border-radius: 0.25rem;
+      transition: all 0.2s ease-in-out;
+      max-width: 100%;
+      height: 300px;
+    }
+
+    .button {
+        background-color: #4e5156; /* DarkGrey */
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: block;
+        margin: 0 auto;
+        width: 20%
+        font-size: 16px;
+    }
+
+    .pt-5,
+    .py-5 {
+      padding-top: 3rem !important;
+    }
+
+    .pb-5,
+    .py-5 {
+      padding-bottom: 3rem !important;
+    }
 
 
+     </style>
+   </head>
 
-<h1 style="font-size:400%;" align="center" style="font-family:'Times New Roman'"> The Collection </h1>
-<style>
-img {
-    display: block;
-    margin: 0 auto;
-}
-</style>
+   <body style="background-color:white">
 
-<img src=http://akgerber.com/wp-content/uploads/Open_Book_Logo_1small.png align="center" alt="BookLogo" style="width:500px;height:350px;">
-<h2 align="center" style="font-family:'Arial'"> Welcome to The Collection</br>
-This is a website for book enthusiast to find out what they should read next!
-</h2>
-<hr>
+    <!--Navigation Bar-->
+     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark navbar-static-top">
+        <ul>
+          <li><a href="#">Menus</a></li>
+          <li><a href="#">Reservations</a></li>
+          <li><a href="#">Past Items</a></li>
+          <li><a href="#">More</a></li>
+        </ul>
+    </nav>
 
-<style>
-.container {
-  border: 2px solid #ccc;
-  background-color: #eee;
-  border-radius: 5px;
-  padding: 16px;
-  margin: 16px;
-}
+    <!--Main Image-->
+      <div class="banner">
+        <img class="banner-image" src="https://wallpaperscraft.com/image/national_day_of_pizza_2015_pizza_cheese_pastries_100087_3840x2160.jpg">
+      </div>
 
-.container::after {
-  content: "";
-  clear: both;
-  display: table;
-}
+      <div class="container">
+          <div class="row top-buffer">
+            <div class="col text-center">
+                <h1>gustoso menus</h1>
+            </div>
+          </div> <!--End First row-->
 
-.container img {
-  float: left;
-  margin-right: 20px;
-  border-radius: 50%;
-}
+          <div class="row">
+            <div class="col text-center">
+            <p>Gustoso Restaurant is a family owned Restaurant that first opened in 1990. It is one of </br>San Francisco's premiere locations for food and
+            excellent service.</p>
+            </div>
+          </div> <!--End Second Row-->
 
-.container span {
-  font-size: 20px;
-  margin-right: 15px;
-}
+          <div class="row">
 
-@media (max-width: 250px) {
-  .container {
-      text-align: center;
-  }
-  .container img {
-      margin: auto;
-      float: none;
-      display: block;
-  }
-}
-</style>
-</head>
-<body>
+            <!--Beer Menu-->
+            <div class="col text-center">
+                <div class="row">
+                    <img src="https://wallpaperscraft.com/image/beer_glasses_two_1391_1920x1080.jpg" alt="beer" class="img-thumbnail">
+                </div>
+                <div class="row text-center">
+                    <h3 style="width:100%">BEER + WINE + COCKTAILS</h3>
+                </div>
+                <div class="row top-buffer-large text-center">
+                    <a href="#" class="button">View Drink Menu</a>
+                </div>
+            </div>
 
-<h2 align="center">Testimonials</h2>
+            <!--Lunch and Dinner Menu-->
+            <div class="col">
+                <div class="row">
+                    <img src="https://i.pinimg.com/originals/36/41/2d/36412d255dcd53a3523dcc1afb5612d2.jpg" alt="pizza" class="img-thumbnail">
+                </div>
+                <div class="row text-center">
+                    <h3 style="width:100%">LUNCH + DINNER MENU</h3>
+                </div>
+                <div class="row top-buffer-large">
+                    <a href="#" class="button">View Dinner Menu</a>
+                </div>
+            </div>
 
-<div class="container" style="background-color: #d0d4db">
-  <img src="http://static.tvtropes.org/pmwiki/pub/images/king_8.png" alt="Stephen King" style="width:90px">
-  <p><span>Stephen King</span>Author</p>
-  <p>The Collection is my go to place to find other great novels.</p>
-</div>
+            <!--Brunch Menu-->
+            <div class="col">
+                <div class="row">
+                    <img src="https://wallpaperscraft.com/image/tea_camomiles_breakfast_croissants_berry_21654_1920x1080.jpg" alt="breakfast" class="img-thumbnail">
+                </div>
+                <div class="row text-center">
+                    <h3 style="width:100%">BRUNCH MENU</h3>
+                </div>
+                <div class="row top-buffer-large">
+                    <a href="#" class="button">View Brunch Menu</a>
+                </div>
+            </div>
 
-<div class="container" style="background-color: #d0d4db">
-  <img src="https://images-na.ssl-images-amazon.com/images/I/51qMV+XJEfL._UX250_.jpg" alt="Danielle Steel" style="width:90px">
-  <p><span >Danielle Steel</span>Author</p>
-  <p>I am happy to see my books on The Collection!</p>
-</div>
+          </div> <!--End Third Row-->
 
-<div class="container" style="background-color: #d0d4db">
-  <img src="http://danbrown.com/wp-content/themes/danbrown/images/db/slideshow/author/db.courter.02.jpg" alt="Stan Brown" style="width:90px">
-  <p><span >Dan $ Brown</span>Author</p>
-  <p>I love the way The Collection organizes its content.</p>
-</div>
+          <div class="row top-buffer-xlarge">
 
-</body>
-</html>
+            <!--Steak Specials-->
+            <div class="col">
+                <div class="row">
+                    <img src="https://wallpaperscraft.com/image/steak_meat_vegetables_dinner_110386_1920x1080.jpg" alt="steak" class="img-thumbnail2">
+                </div>
+                <div class="row text-center">
+                    <h3 style="width:100%">FRIDAY STEAKS</h3>
+                </div>
+                <div class="row top-buffer-large">
+                    <a href="#" class="button">View Steak Specials</a>
+                </div>
+            </div>
+
+            <!--Happy Hour Menu-->
+                <div class="col">
+                    <div class="row">
+                        <img src="https://wallpaperscraft.com/image/mojito_drink_mint_ice_113611_1920x1080.jpg" alt="happyhour" class="img-thumbnail2">
+                    </div>
+                    <div class="row text-center">
+                        <h3 style="width:100%">Happy Hour Menu</h3>
+                    </div>
+                    <div class="row top-buffer-large">
+                        <a href="#" class="button">View Happy Hour Menu</a>
+                    </div>
+                </div>
+
+          </div> <!--End Fourth Row-->
+
+      </div> <!--End Container-->
+
+    <div class="row top-buffer-xlarge">
+        <div class="banner">
+          <img class="banner-image" src="https://images8.alphacoders.com/358/358841.jpg">
+        </div>
+    </div> <!--End Last Row Bottom Banner-->
+
+        <!-- Footer -->
+        <div>
+          <div class="container">
+            <p style="float:left"><b>We Are Located At</b></br>
+            1234 Diablo Lane</br>
+            San Francisco, CA 94121</p>
+            <p style="float:right"><b>Hours</b></br>
+            Monday to Thursday 11am-10pm</br>
+            Friday and Saturday 11am-12pm</br>
+            Closed Sundays</p>
+          </div>
+        </div>
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+   </body>
+ </html>
+
+
 '''
 
 #-------------------- PC Page-------------
